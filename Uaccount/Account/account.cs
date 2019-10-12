9ef7110ca006b43a681d7599a2f7e8ee4107738e
Antony Fagundez <jhon.fagundez@softtek.com>
2019-10-12 03:38:39 -0300

    Account

---
+++ Uaccount/Account/account.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace account
+{
+    private class Account
+    {
+        String bank;
+        Double amount;
+        String CBU;
+        string Type;
+    }
+}
